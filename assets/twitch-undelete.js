2f052f80a9a7b0e6b35a821afa54b0b701218cfc
wingedseraph <r89647@gmail.com>
2025-09-13 15:09:49 +0300

    feat: twitch msg

--- assets/twitch-undelete.js
+++ assets/twitch-undelete.js
@@ -1,179 +1,174 @@
 //@ts-check
-var messageObservers = []
-var removedMessages = {}
-var getId = idMaker()
+var messageObservers = [];
+var removedMessages = {};
+var getId = idMaker();
 
 /**
  * Finds the chat container
  */
 document.onreadystatechange = function () {
   let interval = setInterval(function () {
-    let amsg = document
-      .querySelector('.chat-line__message')
+    let amsg = document.querySelector(".chat-line__message");
 
+    console.log(amsg);
     if (!amsg) {
-      return
+      return;
     }
 
-    clearInterval(interval)
-    let chat = amsg.parentElement
+    clearInterval(interval);
+    let chat = amsg.parentElement;
     getChatObserver().observe(chat, {
-      childList: true
-    })
-  }, 500)
+      childList: true,
+    });
+  }, 500);
 
   // Add some styling to the undeleted messages
-  addCss()
-}
+  addCss();
+};
 
 /**
  * Adds a message observer for every new message.
  * Keeps at most 30 observers running at one time,
  * which is approximately 1-2x the number of messages
  * visible at a time on a 1080p screen.
- * 
+ *
  * @param {Mutation} mutation
  */
 function handleNewMessage(mutation) {
   if (mutation.addedNodes.length == 0) {
-    return
+    return;
   }
-  let message = mutation.addedNodes[0]
-  let moid = getId.next().value
-  let messageObserver = getMessageObserver()
+  let message = mutation.addedNodes[0];
+  console.log(message);
+  let moid = getId.next().value;
+  let messageObserver = getMessageObserver();
   messageObserver.observe(message, {
     childList: true,
-    subtree: true
-  })
-  messageObservers.push(messageObserver)
+    subtree: true,
+  });
+  messageObservers.push(messageObserver);
 
   if (messageObservers.length > 30) {
-    let oldest = messageObservers.shift()
-    oldest.disconnect()
+    let oldest = messageObservers.shift();
+    oldest.disconnect();
   }
 }
 
 /**
  * Replaces deleted messages
  * with their original data
- * 
- * @param {Mutation} mutation 
+ *
+ * @param {Mutation} mutation
  */
 function handleMessageModified(mutation) {
-  let id = mutation.target.dataset.tuid
-  let addedMessage = mutation.addedNodes[0]
+  let id = mutation.target.dataset.tuid;
+  let addedMessage = mutation.addedNodes[0];
 
-  if (!id || !addedMessage.className.includes('deleted')) {
-    return
+  if (!id || !addedMessage.className.includes("deleted")) {
+    return;
   }
 
-  let originalMessage = removedMessages[id]
-  addedMessage.innerHTML = originalMessage.innerHTML
-  addedMessage.classList.add('tu-re-added')
-  delete removedMessages[id]
+  let originalMessage = removedMessages[id];
+  addedMessage.innerHTML = originalMessage.innerHTML;
+  addedMessage.classList.add("tu-re-added");
+  delete removedMessages[id];
 
   // hack to avoid unforseen growth of map
   if (Object.keys(removedMessages).length > 10) {
-    removedMessages = {}
+    removedMessages = {};
   }
 }
 
 /**
  * Stores removed messages
  * so they can be restored
- * 
- * @param {Mutation} mutation 
+ *
+ * @param {Mutation} mutation
  */
 function handleMessageRemoved(mutation) {
-  let removedMessage = mutation.removedNodes[0]
+  let removedMessage = mutation.removedNodes[0];
   if (!isMessage(removedMessage)) {
-    return false
+    return false;
   }
-  let tuid = getId.next().value
-  removedMessages[tuid] = removedMessage
-  mutation.target.dataset.tuid = mutation.target.dataset.tuid || tuid
+  let tuid = getId.next().value;
+  removedMessages[tuid] = removedMessage;
+  mutation.target.dataset.tuid = mutation.target.dataset.tuid || tuid;
 }
 
 /**
  * Checks if the removed node
  * is actually a chat message
- * 
+ *
  * @param {Node} removedMessage
  * @returns Boolean
  */
 function isMessage(removedMessage) {
-  let invalid = (
+  let invalid =
     !removedMessage.innerHTML ||
     removedMessage.innerHTML.length < 2 ||
-    removedMessage.innerHTML.includes('placeholder') ||
-    removedMessage.className.includes('deleted') ||
-    removedMessage.className.includes('tooltip')
-  )
+    removedMessage.innerHTML.includes("placeholder") ||
+    removedMessage.className.includes("deleted") ||
+    removedMessage.className.includes("tooltip");
 
-  return !invalid
+  return !invalid;
 }
 
 /**
  * Factory for the chat observer
  */
 function getChatObserver() {
-  return new MutationObserver(
-    function (mutationsList) {
-      for (let mutation of mutationsList) {
-        if (mutation.type === 'childList') {
-          handleNewMessage(mutation)
-        }
+  return new MutationObserver(function (mutationsList) {
+    for (let mutation of mutationsList) {
+      if (mutation.type === "childList") {
+        handleNewMessage(mutation);
       }
     }
-  )
+  });
 }
 
 /**
  * Factory for message observers
  */
 function getMessageObserver() {
-  return new MutationObserver(
-    function (mutationsList) {
-      for (let mutation of mutationsList) {
-        let relevant
-        // Removed message
-        if (mutation.removedNodes.length == 1) {
-          relevant = handleMessageRemoved(mutation)
-          if (!relevant) {
-            continue
-          }
-        }
-        // Added message
-        if (mutation.addedNodes.length == 1) {
-          handleMessageModified(mutation)
+  return new MutationObserver(function (mutationsList) {
+    for (let mutation of mutationsList) {
+      let relevant;
+      // Removed message
+      if (mutation.removedNodes.length == 1) {
+        relevant = handleMessageRemoved(mutation);
+        if (!relevant) {
+          continue;
         }
       }
+      // Added message
+      if (mutation.addedNodes.length == 1) {
+        handleMessageModified(mutation);
+      }
     }
-  )
+  });
 }
 
 /**
  * Adds styling to undeleted messages
  */
 function addCss() {
-  let style = document.createElement('style')
+  let style = document.createElement("style");
   style.innerHTML = `
     .tu-re-added {
       font-weight: bold !important;
       color: red !important;
     }
-  `
-  document
-    .getElementsByTagName('head')[0]
-    .appendChild(style)
+  `;
+  document.getElementsByTagName("head")[0].appendChild(style);
 }
 
 /**
  * Generates ID's for removed messages
  */
 function* idMaker() {
-  let index = 0
+  let index = 0;
   while (index < index + 1) {
-    yield index++
+    yield index++;
   }
 }
+
